0eb0b73dc428f12b7f9888c592b801e6f35bb6cf
KOROBYAKA <109600101+KOROBYAKA@users.noreply.github.com>
2022-09-14 12:05:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from os import system
 from sys import exit
+
 from mininet.node import Host
 from mininet.topo import Topo
 from mininet.util import quietRun
 from mininet.log import error
-
-
-
+import sys
+from functools import partial
+from mininet.node import Controller
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.topo import SingleSwitchTopo
+from mininet.log import setLogLevel
 class VLANHost( Host ):
     "Host connected to VLAN interface"
 
@@ -30,22 +36,51 @@ class VLANHost( Host ):
 
 hosts = { 'vlan': VLANHost }
 
+
+
+
 class MyTopo( Topo ):
-    def build( self, k=4, n=1, vlanBase=100 ):
-        s1 = self.addSwitch( 's1' )
-        for i in range( k ):
-            vlan = vlanBase + i
-        name = 'hA'
-        hA = self.addHost( name, cls=VLANHost, vlan=vlan )
-        self.addLink( hA, s1)
 
-        hB = self.addHost( 'hB')
-        self.addLink( hB, s1 )
-        self.addLink(hB, s1)
-        self.addLink(hB, s1)
-        self.addLink(hB, s1)
+    def build( self, vlanBase=100 ):
+        s1 = self.addSwitch('s1')
+        hB = self.addHost('hB')
 
+        vlan = vlanBase
+        hA = self.addHost('hA',cls=VLANHost,vlan=vlan)
 
-
+        x = 1
+        for x in range (4):
+            self.addLink('hB', 's1')
+        self.addLink('hA', 's1')
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
+
+def test():
+    net = Mininet(topo=MyTopo(), waitConnected=True)
+    net.start()
+
+
+
+
+    hA = net['hA']
+
+    for x in range (0,4):
+        hA.cmd(f'sudo ip link add link hA-eth0 name hA-eth0.10{x} type vlan id 10{x}')
+        hA.cmd(f'ip addr add 10.0.0.{x}/24 brd 10.0.0.255 dev hA-eth0.10{x}')
+        hA.cmd(f'ip link set dev hA-eth0.10{x} up')
+     #   s1.cmd('')
+      #  s1.cmd('')
+       # s1.cmd('')
+    CLI(net)
+    net.stop()
+def main():
+
+   
+
+    test()
+
+
+
+
+if __name__ == "__main__":
+    main()
